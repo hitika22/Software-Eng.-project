310a97ac7ebffb0d103c2584e487c461528ab150
hitika22 <70220300+hitika22@users.noreply.github.com>
2021-04-06 19:32:31 +0530

    Update registration.dart

--- swe_flutter/flu/lib/registration.dart
+++ swe_flutter/flu/lib/registration.dart
@@ -7,6 +7,8 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final GlobalKey<FormFieldState<String>> _passwordFieldKey =
+  GlobalKey<FormFieldState<String>>();
 
   final _auth=FirebaseAuth.instance;
   User loggedInUser;
@@ -31,179 +33,310 @@ class _RegisterState extends State<Register> {
   String password='';
   String username='';
   String phonenumber='';
+  String yoe='';
+  String position='';
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Center(child: Text("REGISTER HERE ")),
+        title: Center(
+          child:
+          Text(
+            "REGISTER HERE ",
+            style: TextStyle(
+              color: Colors.pink.shade50,
+              fontWeight: FontWeight.w900,
+            )
+        ),
+
+
+        ),
+
       ),
       body: Stack(
           children: [
       Container(
+
       decoration: BoxDecoration(
       gradient: LinearGradient(
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
           colors: <Color>[
-            Color(0xFF6E14FF),
-            Color(0xFF5F5F5F)
+            Color(0xFF6E15FF),
+            Color(0xFF720094)
           ])
     ),
     ),
-      Column(
-        children: [
 
-          Expanded(
+      Container(
+        padding: EdgeInsets.all(20),
+        margin: const EdgeInsets.only(left: 200.0,right: 200.0),
+        // color: Colors.white,
+        decoration: BoxDecoration(
+            color: Colors.purple.shade50,
+            borderRadius: BorderRadius.circular(25)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
 
-            child: Container(),),
-          Expanded(
-            flex: 2,
-            child: Row(
-              children: [
-                Expanded(child: Container()),
-                Expanded(
-                  flex: 2,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        textCapitalization: TextCapitalization.words,
-                        // controller: cname,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          filled: true,
-                          icon: Icon(Icons.person,
-                            color: Colors.white,
+          children: [
+            // CircleAvatar(radius: (50),
+            //     backgroundColor: Colors.white,
+            //     child: ClipRRect(
+            //       borderRadius:BorderRadius.circular(50),
+            //       child: Image.asset("images/register.png"),
+            //     )
+            // ),
+            // SizedBox(
+            //   height: 25,
+            // ),
 
-                          ),
+            TextFormField(
+              textCapitalization: TextCapitalization.words,
+              // controller: cname,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                filled: true,
+                icon: Icon(Icons.person,
+                  color: Colors.purple,
 
-                          hintText: 'What do people call you?',
-                          hintStyle: TextStyle( color: Colors.white),
-                          labelText: 'Name *',
-                          labelStyle: TextStyle( color: Colors.white),
-                        ),
-                        onSaved: (String value) {
-                          username=value;
-                        },
-                        onChanged: (String value) {
-                          username=value;
-                        },
-                        // validator: _validateName,
-                      ),
-                      const SizedBox(height: 24.0),
-                      TextFormField(
-                        // controller: caddress,
-                        textCapitalization: TextCapitalization.words,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          filled: true,
-                          icon: Icon(Icons.email,
-                            color: Colors.white,),
-                          hintText: 'Enter your Email?',
-                          hintStyle: TextStyle( color: Colors.white),
-                          labelText: 'emai Address *',
-                          labelStyle: TextStyle( color: Colors.white),
-                        ),
-                        onSaved: (String value) {
-                          email=value;
-                        },
-                        onChanged: (String value){
-                          email=value;
-                        },
-                        // validator: _validateName,
-                      ),
-                      const SizedBox(height: 24.0),
-                      // "Phone number" form.
-                      TextFormField(
-                        // controller: cphoneNumber,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          filled: true,
-                          icon: Icon(Icons.phone,
-                            color: Colors.white,),
-                          hintText: 'Where can we reach you?',
-                          hintStyle: TextStyle( color: Colors.white),
-                          labelText: 'Phone Number *',
-                          labelStyle: TextStyle( color: Colors.white),
-                          prefixText: '+91',
-                        ),
-                        keyboardType: TextInputType.phone,
-                        onSaved: (String value) {
-                          phonenumber=value;
-                        },
-                        onChanged: (String value) {
-                          phonenumber=value;
-                        },
-                        // TextInputFormatters are applied in sequence.
-                      ),
-                      const SizedBox(height: 24.0),
-                      // "Phone number" form.
-                      TextFormField(
-                        // controller: cphoneNumber,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          filled: true,
-                          icon: Icon(Icons.security,
-                            color: Colors.white,),
-                          hintText: 'Where can we reach you?',
-                          hintStyle: TextStyle( color: Colors.white),
-                          labelText: 'Password *',
-                          labelStyle: TextStyle( color: Colors.white),
-                        ),
-                        keyboardType: TextInputType.phone,
-                        onSaved: (String value) {
-                          password=value;
-                        },
-                        onChanged: (String value) {
-                          password=value;
-                        },
-                        // TextInputFormatters are applied in sequence.
-                      ),
-
-                      const SizedBox(height: 24.0),
-                      RaisedButton(
-                        onPressed: () async {
-                          try {
-                            final newUser=await _auth.createUserWithEmailAndPassword(email: email, password: password);
-
-                            CollectionReference users =FirebaseFirestore.instance.collection('users');
-                            User user=FirebaseAuth.instance.currentUser;
-                            DocumentSnapshot documentSnapshot= await users.doc(user.uid).get();
-
-                            if(!documentSnapshot.exists){
-                              users.doc(user.uid).set({
-                                "id": user.uid,
-                                "name": username,
-                                "email": email,
-                                "phonenumber": phonenumber,
-                                "isdev": false,
-                                "isdoct": false,
-                              });
-                            }
-                            if(newUser!=null){
-                              Navigator.pushNamed(context, '/');
-                            }
-                          }
-                          catch(e){
-
-                          }
-                        },
-                        child: Text('Submit'),
-                      ),
-                    ],
-                  ),
                 ),
-                Expanded(child: Container()),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(),),
-        ],
 
+                hintText: 'What do people call you?',
+                hintStyle: TextStyle( color: Colors.black),
+                labelText: 'Name *',
+                labelStyle: TextStyle( color: Colors.black),
+              ),
+              onSaved: (String value) {
+                username=value;
+              },
+              onChanged: (String value) {
+                username=value;
+              },
+              // validator: _validateName,
+            ),
+            const SizedBox(height: 24.0),
+            TextFormField(
+              // controller: caddress,
+              textCapitalization: TextCapitalization.words,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                filled: true,
+                icon: Icon(Icons.email,
+                  color: Colors.purple),
+                hintText: 'Enter your Email?',
+                hintStyle: TextStyle( color: Colors.black),
+                labelText: 'email Address *',
+                labelStyle: TextStyle( color: Colors.black),
+              ),
+              onSaved: (String value) {
+                email=value;
+              },
+              onChanged: (String value){
+                email=value;
+              },
+              // validator: _validateName,
+            ),
+            const SizedBox(height: 24.0),
+            // "Phone number" form.
+            TextFormField(
+              // controller: cphoneNumber,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                filled: true,
+                icon: Icon(Icons.phone,
+                  color: Colors.purple,),
+                hintText: 'Where can we reach you?',
+                hintStyle: TextStyle( color: Colors.black),
+                labelText: 'Phone Number *',
+                labelStyle: TextStyle( color: Colors.black),
+                prefixText: '+91',
+              ),
+              keyboardType: TextInputType.phone,
+
+              onSaved: (String value) {
+                phonenumber=value;
+              },
+              onChanged: (String value) {
+                phonenumber=value;
+              },
+              // TextInputFormatters are applied in sequence.
+            ),
+
+            const SizedBox(height: 24.0),
+
+            TextFormField(
+              // controller: cphoneNumber,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                filled: true,
+                icon: Icon(Icons.calendar_today,
+                  color: Colors.purple,),
+                hintText: 'Enter Years Of Experience!',
+                hintStyle: TextStyle( color: Colors.black),
+                labelText: 'YOE *',
+                labelStyle: TextStyle( color: Colors.black),
+
+              ),
+              keyboardType: TextInputType.number,
+
+              onSaved: (String value) {
+                yoe=value;
+              },
+              onChanged: (String value) {
+                yoe=value;
+              },
+              // TextInputFormatters are applied in sequence.
+            ),
+
+            const SizedBox(height: 24.0),
+
+            TextFormField(
+              // controller: cphoneNumber,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                filled: true,
+                icon: Icon(Icons.medical_services_sharp,
+                  color: Colors.purple,),
+                hintText: 'Enter your specification?',
+                hintStyle: TextStyle( color: Colors.black),
+                labelText: 'Position *',
+                labelStyle: TextStyle( color: Colors.black),
+
+              ),
+
+
+              onSaved: (String value) {
+                position=value;
+              },
+              onChanged: (String value) {
+                position=value;
+              },
+              // TextInputFormatters are applied in sequence.
+            ),
+
+            const SizedBox(height: 24.0),
+
+
+            // "Phone number" form.
+            PasswordField(
+              fieldKey: _passwordFieldKey,
+
+              helperText: 'Not more than 8 characters.',
+              labelText: 'Password *',
+
+
+              onFieldSubmitted: (String value) {
+                setState(() {
+                  this.password = value;
+                });
+              },
+            ),
+            TextFormField(
+              enabled: this.password != null &&
+                  this.password.isNotEmpty,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                filled: true,
+                icon: Icon(Icons.security,
+                color: Colors.purple,),
+                labelText: 'Re-type password *',
+              ),
+              maxLength: 8,
+              obscureText: true,
+            ),
+
+            const SizedBox(height: 24.0),
+
+            RaisedButton(
+              onPressed: () async {
+                try {
+                  final newUser=await _auth.createUserWithEmailAndPassword(email: email, password: password);
+
+                  CollectionReference users =FirebaseFirestore.instance.collection('users');
+                  User user=FirebaseAuth.instance.currentUser;
+                  DocumentSnapshot documentSnapshot= await users.doc(user.uid).get();
+
+                  if(!documentSnapshot.exists){
+                    users.doc(user.uid).set({
+                      "id": user.uid,
+                      "name": username,
+                      "email": email,
+                      "phonenumber": phonenumber,
+                      "yoe":yoe,
+                      "position":position,
+                      "isdev": false,
+                      "isdoct": false,
+                    });
+                  }
+                  if(newUser!=null){
+                    Navigator.pushNamed(context, '/');
+                  }
+                }
+                catch(e){
+
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        ),
       ),
     ],
           ),
     );
 
+  }
+}
+class PasswordField extends StatefulWidget {
+  const PasswordField({
+    this.fieldKey,
+    this.hintText,
+    this.labelText,
+    this.helperText,
+    this.onSaved,
+    this.validator,
+    this.onFieldSubmitted,
+  });
+
+  final Key fieldKey;
+  final String hintText;
+  final String labelText;
+  final String helperText;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
+  final ValueChanged<String> onFieldSubmitted;
+
+  @override
+  _PasswordFieldState createState() => _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      key: widget.fieldKey,
+      obscureText: _obscureText,
+      maxLength: 8,
+      onSaved: widget.onSaved,
+      validator: widget.validator,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      decoration: InputDecoration(
+        border: const UnderlineInputBorder(),
+        filled: true,
+        hintText: widget.hintText,
+        labelText: widget.labelText,
+        helperText: widget.helperText,
+        suffixIcon: GestureDetector(
+          onTap: () {
+            setState(() {
+              _obscureText = !_obscureText;
+            });
+          },
+          child: Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+        ),
+      ),
+    );
   }
 }
